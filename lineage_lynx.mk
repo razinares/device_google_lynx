fc8bb45f59b8abb74eb0b01869db846d7d6ccf84
razinhossain008 <razinhossain008@gmail.com>
2024-09-12 15:05:57 +0000

    Init @EverestOS

--- lineage_lynx.mk
+++ lineage_lynx.mk
@@ -18,6 +18,32 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 7a
 PRODUCT_NAME := lineage_lynx
 
+# Maintainer name for Everest
+EVEREST_MAINTAINER := "RAZIN"
+
+# For UDFPS devices
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+
+# Build GAPPS\Vanilla
+WITH_GAPPS := true
+
+# Quick switch (add more than one Launcher in build)
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+
+TARGET_IS_PIXEL := true
+TARGET_IS_PIXEL_7 := true
+
+# Pixel Launcher Recent Blur
+TARGET_USES_BLUR_RECENT := false
+
+# Blur Support 
+TARGET_SUPPORTS_BLUR := true
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
